ba643e6e00d3072e488c9a649b296bf3217d55a1
mjnwnabw174-byte <mjnwnabw174@gmail.com>
2026-07-18 23:00:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import telebot
-from config import BOT_TOKEN
 
+from config import BOT_TOKEN, check_config
+from database import create_tables
+from handlers import register_handlers
+
+
+# فحص الإعدادات
+check_config()
+
+# إنشاء البوت
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(
-        message.chat.id,
-        "👋 أهلاً بك في بوت\n"
-        "✍📄 الترند بنفسك\n\n"
-        "🚀 اصنع ترندك وشارك روابطك بسهولة."
-    )
+# تجهيز قاعدة البيانات
+create_tables()
 
 
-print("✅ البوت يعمل الآن...")
+# تشغيل جميع أوامر البوت والأزرار
+register_handlers(bot)
 
+
+print("✅ ✍📄 الترند بنفسك يعمل الآن...")
+
+
+# تشغيل البوت
 bot.infinity_polling()
